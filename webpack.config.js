5e985f5f040f18759831d6900983e1cffe072349
robotic-monkey <seanpatmiller@gmail.com>
2018-04-10 16:58:45 -0800

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -12,35 +12,34 @@ module.exports = (env) => {
       filename: 'bundle.js'
     },
     module: {
-      rules: [
-        {
-          loader: 'babel-loader',
-          test: /\.js$/,
-          exclude: /node_modules/
-        }, {
-          test: /\.s?css$/,
-          use: CSSExtract.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true
-                }
-              }, {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: true
-                }
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
               }
-            ]
-          })
-        }
-      ]
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
     },
-    plugins: [CSSExtract],
-    devtool: isProduction
-      ? 'source-map'
-      : 'inline-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
